b560021f4b82dde62469f8017ee73b40983aecc7
veryneatperson <fourbillionyearsofevolution@gmail.com>
2022-03-29 22:59:55 +0300

    [IMP-4] Creating Header component.

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
+import Header from './components/Header';
+
 function App() {
   return (
     <div className="App">
-      <div className="container"></div>
+      <div className="container">
+        <Header />
+      </div>
     </div>
   );
 }
---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,11 @@
+import { ReactComponent as AviasalesLogo } from './assets/logo.svg';
+
+import './Header.scss';
+
+export default function Header() {
+  return (
+    <header className="header">
+      <AviasalesLogo />
+    </header>
+  );
+}

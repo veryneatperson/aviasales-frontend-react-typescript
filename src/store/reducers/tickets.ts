4a4e42ed9974fb430251eabb6a89a725981386e2
veryneatperson <fourbillionyearsofevolution@gmail.com>
2022-03-30 16:07:15 +0300

    [IMP-5] Describing tickets.

---
+++ src/store/reducers/tickets.ts
@@ -0,0 +1,51 @@
+import {
+  TicketsActionTypes,
+  TicketsState,
+  TicketsAction,
+} from '../../types/tickets';
+
+export const initState: TicketsState = {
+  searchId: '',
+  tickets: [],
+  isLoading: false,
+  searchIdError: false,
+  ticketsError: false,
+  fetchedAllTickets: false,
+};
+
+export const reducer = (
+  state: TicketsState,
+  action: TicketsAction
+): TicketsState => {
+  switch (action.type) {
+    case TicketsActionTypes.GET_SEARCHID_SUCCESS:
+      return {
+        ...state,
+        searchId: action.payload,
+      };
+    case TicketsActionTypes.GET_SEARCHID_FAILURE:
+      return {
+        ...state,
+        isLoading: false,
+        searchIdError: true,
+      };
+    case TicketsActionTypes.GET_TICKETS_REQUEST:
+      return {
+        ...state,
+        isLoading: true,
+      };
+    case TicketsActionTypes.GET_TICKETS_SUCCESS:
+      return {
+        ...state,
+        tickets: [...state.tickets, ...action.payload.tickets],
+        fetchedAllTickets: action.payload.stop,
+      };
+    case TicketsActionTypes.GET_TICKETS_FAILURE:
+      return {
+        ...state,
+        ticketsError: true,
+      };
+    default:
+      return state;
+  }
+};
---
+++ src/types/tickets.ts
@@ -0,0 +1,68 @@
+export interface TicketsState {
+  searchId: string;
+  tickets: Ticket[] | [];
+  isLoading: boolean;
+  searchIdError: boolean;
+  ticketsError: boolean;
+  fetchedAllTickets: boolean;
+}
+
+export enum TicketsActionTypes {
+  GET_SEARCHID_SUCCESS = 'GET_SEARCHID_SUCCESS',
+  GET_SEARCHID_FAILURE = 'GET_SEARCHID_FAILURE',
+  GET_TICKETS_REQUEST = 'GET_TICKETS_REQUEST',
+  GET_TICKETS_SUCCESS = 'GET_TICKETS_SUCCESS',
+  GET_TICKETS_FAILURE = 'GET_TICKETS_FAILURE',
+}
+
+export interface TicketsResponse {
+  tickets: Ticket[];
+  stop: boolean;
+}
+
+export interface SearchIdResponse {
+  searchId: string;
+}
+
+interface GetSearchIdSuccessAction {
+  type: TicketsActionTypes.GET_SEARCHID_SUCCESS;
+  payload: string;
+}
+
+interface GetSearchIdFailureAction {
+  type: TicketsActionTypes.GET_SEARCHID_FAILURE;
+}
+
+interface GetTicketsRequestAction {
+  type: TicketsActionTypes.GET_TICKETS_REQUEST;
+}
+
+interface GetTicketsSuccessAction {
+  type: TicketsActionTypes.GET_TICKETS_SUCCESS;
+  payload: TicketsResponse;
+}
+
+interface GetTicketsFailureAction {
+  type: TicketsActionTypes.GET_TICKETS_FAILURE;
+}
+
+export type TicketsAction =
+  | GetSearchIdSuccessAction
+  | GetSearchIdFailureAction
+  | GetTicketsRequestAction
+  | GetTicketsSuccessAction
+  | GetTicketsFailureAction;
+
+interface Segment {
+  origin: string;
+  destination: string;
+  date: string;
+  stops: string[];
+  duration: number;
+}
+
+export interface Ticket {
+  price: number;
+  carrier: string;
+  segments: [Segment, Segment];
+}

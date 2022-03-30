b9a3874239e9ef4d0ff084e049f442ad6f771002
veryneatperson <fourbillionyearsofevolution@gmail.com>
2022-03-30 16:30:50 +0300

    [IMP-5] Adding constants.

---
+++ src/constants/index.ts
@@ -0,0 +1,4 @@
+export const BASE_URL = 'https://front-test.beta.aviasales.ru';
+export const BASE_CARRIER_LOGO_URL = '//pics.avs.io/99/36/';
+export const SEARCH_ID_ERROR_MSG = 'Не удалось получить идентификатор поиска билетов';
+export const TICKETS_ERROR_MSG = 'Не удалось загрузить билеты';

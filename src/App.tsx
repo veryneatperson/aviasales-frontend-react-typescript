86c3b0035ce3fa4e303c38945d67fa8193ca30ae
veryneatperson <fourbillionyearsofevolution@gmail.com>
2022-04-01 13:12:33 +0300

    [IMP-13] Creating App component.

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,36 @@
+import { useReducer } from 'react';
+
 import Header from './components/Header';
+import LayoverFilters from './components/LayoverFilters';
+import SortingTabs from './components/SortingTabs';
+import TicketList from './components/TicketList';
+
+import {
+  initState as groupingInitState,
+  reducer as groupingReducer,
+} from './store/reducers/grouping';
+
+import './App.scss';
 
 function App() {
+  const [groupingState, groupingDispatch] = useReducer(
+    groupingReducer,
+    groupingInitState
+  );
+
+  const { layovers, sorting } = groupingState;
+
   return (
     <div className="App">
       <div className="container">
         <Header />
+        <div className="wrapper">
+          <LayoverFilters layovers={layovers} dispatch={groupingDispatch} />
+          <div className="main">
+            <SortingTabs sorting={sorting} dispatch={groupingDispatch} />
+            <TicketList sorting={sorting} layovers={layovers} />
+          </div>
+        </div>
       </div>
     </div>
   );
